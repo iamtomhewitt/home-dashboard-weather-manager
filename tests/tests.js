71d3273b4e0f45c573c27e7f7d9f0718d2ec4359
Tom Hewitt <tommhewitt@gmail.com>
2020-02-15 15:12:32 +0000

    🧪 Update tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const assert = require('assert');
+require('dotenv').config();
 
 describe('/ tests', () => {
     let server;
@@ -25,6 +26,74 @@ describe('/ tests', () => {
 
                 assert.equal(response.body.version, version);
                 assert.equal(response.body.status, status);
+
+                return done();
+            });
+    });
+});
+
+describe('/weather tests', () => {
+    let server;
+
+    before(() => {
+        server = require('../app').listen(3002);
+    });
+
+    after(() => {
+        server.close();
+    });
+
+    it('/weather gives 200', (done) => {
+        request(server)
+            .get(`/weather?latitude=&longitude=&apiKey=${process.env.API_KEY}`)
+            .expect(200)
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.notEqual(response.body.current, null);
+                assert.notEqual(response.body.current.summary, null);
+                assert.notEqual(response.body.current.icon, null);
+                assert.notEqual(response.body.current.temperature, null);
+
+                assert.notEqual(response.body.week, null);
+                assert.notEqual(response.body.week[0].day, null);
+                assert.notEqual(response.body.week[0].icon, null);
+                assert.notEqual(response.body.week[0].temperatureLow, null);
+                assert.notEqual(response.body.week[0].temperatureHigh, null);
+
+                return done();
+            });
+    });
+
+    it('/weather gives 400 if query parameters are missing', (done) => {
+        request(server)
+            .get('/weather')
+            .expect(400)
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.equal(response.body.status, 400);
+                assert.equal(response.body.message, 'There are missing query parameters');
+
+                return done();
+            });
+    });
+
+    it('/weather gives 401 if API key is incorrect', (done) => {
+        request(server)
+            .get('/weather?latitude=10&longitude=5&apiKey=incorrect')
+            .expect(401)
+            .end((err, response) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.equal(response.body.status, 401);
+                assert.equal(response.body.message, 'API key is incorrect');
 
                 return done();
             });
