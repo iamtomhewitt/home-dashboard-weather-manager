9e40eb507b8da46d690821eb2e1a0b48bf062af1
Tom Hewitt <tommhewitt@gmail.com>
2020-02-15 15:17:23 +0000

    💻 Handle missing parameters

--- app.js
+++ app.js
@@ -8,13 +8,31 @@ const app = express();
 
 const success = 200;
 const badRequest = 400;
+const unauthorised = 401;
 const serverError = 500;
+
+function response(status, message) {
+    return {
+        status,
+        message,
+    };
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.status(success).send({ status: '🌞 SERVER OK', version, endpoints: listEndpoints(app) });
+});
+
+app.get('/weather', (req, res) => {
+    const { latitude } = req.query;
+    const { longitude } = req.query;
+    const { apiKey } = req.query;
+
+    if (!latitude || !longitude || !apiKey) {
+        res.status(badRequest).send(response(badRequest, 'There are missing query parameters'));
+    }
 });
 
 const port = 3001;
